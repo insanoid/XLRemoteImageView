f905863fb614d2dda5a3edc8fdf0892333aee2c6
Karthikeya Udupa K M <karthikeyaudupa@gmail.com>
2016-08-02 14:12:53 +0200

    removed afnetworking

--- XLRemoteImageView/XLRemoteImageView/ViewController.m
+++ XLRemoteImageView/XLRemoteImageView/ViewController.m
@@ -6,17 +6,14 @@
 //  Copyright (c) 2013 Xmartlabs. All rights reserved.
 //
 
-#import "ViewController.h"
 #import "UIImageView+XLNetworking.h"
 #import "UIImageView+XLProgressIndicator.h"
-
-#import <AFNetworking/UIImageView+AFNetworking.h>
+#import "ViewController.h"
 
 @interface ViewController ()
 
 @property (nonatomic) UIImageView *imageView;
 @property int countRefresh;
-
 
 @end
 
@@ -25,33 +22,28 @@
 @synthesize imageView = _imageView;
 @synthesize countRefresh = _countRefresh;
 
-
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-
+    // Do any additional setup after loading the view, typically from a nib.
 }
 
--(void)viewWillAppear:(BOOL)animated
-{
+- (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-    [self.view setBackgroundColor: [UIColor whiteColor]];
+    [self.view setBackgroundColor:[UIColor whiteColor]];
     [self.view addSubview:self.imageView];
     [self refreshImage:nil];
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-
 #pragma mark - properties
 
--(UIImageView *)imageView{
-    if (_imageView) return _imageView;
+- (UIImageView *)imageView {
+    if (_imageView)
+        return _imageView;
     _imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, self.navigationController.navigationBar.frame.size.height + [UIApplication sharedApplication].statusBarFrame.size.height, self.view.bounds.size.width, 320)];
     [_imageView setBackgroundColor:[UIColor colorWithRed:0.84 green:0.85 blue:0.86 alpha:0.9f]];
     _imageView.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin;
@@ -60,17 +52,13 @@
     return _imageView;
 }
 
-
 #pragma mark - example
 
 - (IBAction)refreshImage:(UIBarButtonItem *)sender {
     // use another url to prevent cache usage
-    NSString * url = [NSString stringWithFormat:@"https://raw.githubusercontent.com/xmartlabs/XLRemoteImageView/master/screenshot.png?countRefresh=%i", self.countRefresh];
+    NSString *url = [NSString stringWithFormat:@"https://farm1.staticflickr.com/29/55141873_46dbf53454_o_d.jpg?countRefresh=%i", self.countRefresh];
     self.countRefresh += 1;
-    
     [self.imageView setImageWithProgressIndicatorAndURL:[NSURL URLWithString:url]];
 }
-
-
 
 @end
--- XLRemoteImageView/XLRemoteImageView/XL/UIImageView+XLNetworking.m
+++ XLRemoteImageView/XLRemoteImageView/XL/UIImageView+XLNetworking.m
@@ -7,11 +7,10 @@
 //
 
 #import "UIImageView+XLNetworking.h"
-#import <AFNetworking/AFNetworking.h>
-#import <AFNetworking/UIImageView+AFNetworking.h>
 #import "XLCircleProgressIndicator.h"
+#import <SDWebImageDownloader.h>
+#import <UIImageView+WebCache.h>
 #import <objc/message.h>
-
 
 @interface AFImageCache : NSCache
 - (UIImage *)cachedImageForRequest:(NSURLRequest *)request;
@@ -19,23 +18,7 @@
         forRequest:(NSURLRequest *)request;
 @end
 
-@interface UIImageView (_XLNetworking)
-
-@property (readwrite, nonatomic, strong, setter = af_setImageRequestOperation:) AFHTTPRequestOperation *af_imageRequestOperation;
-
-@end
-
-@implementation UIImageView (_XLNetworking)
-
-@dynamic af_imageRequestOperation;
-
-@end
-
-
-
 @implementation UIImageView (XLNetworking)
-
-
 
 - (void)setImageWithURLRequest:(NSURLRequest *)urlRequest
               placeholderImage:(UIImage *)placeholderImage
@@ -44,73 +27,20 @@
          downloadProgressBlock:(void (^)(NSUInteger bytesRead, long long totalBytesRead, long long totalBytesExpectedToRead))downloadProgressBlock
 
 {
-    [self cancelImageRequestOperation];
-
-    // get AFNetworking UIImageView cache
-    AFImageCache *cache = (AFImageCache *)((id (*)(id, SEL))objc_msgSend)([self class], @selector(sharedImageCache));
-    // try to get the image from cache
-    UIImage *cachedImage = [cache cachedImageForRequest:urlRequest];
-    if (cachedImage) {
-        if (success) {
-            success(nil, nil, cachedImage);
-        } else {
-            dispatch_async(dispatch_get_main_queue(), ^(void) {
-              self.image = cachedImage;
-            });
+    [self sd_cancelCurrentImageLoad];
+    SDWebImageManager *manager = [SDWebImageManager sharedManager];
+    __block long long totalBytesRead = 0;
+    [manager downloadImageWithURL:urlRequest.URL
+        options:0
+        progress:^(NSInteger receivedSize, NSInteger expectedSize) {
+          totalBytesRead = totalBytesRead + receivedSize;
+          downloadProgressBlock(receivedSize, totalBytesRead, expectedSize);
         }
-
-        self.af_imageRequestOperation = nil;
-    } else {
-        if (placeholderImage) {
-            dispatch_async(dispatch_get_main_queue(), ^(void) {
-              self.image = placeholderImage;
-            });
-        }
-
-        __weak __typeof(self) weakSelf = self;
-        self.af_imageRequestOperation = [[AFHTTPRequestOperation alloc] initWithRequest:urlRequest];
-        self.af_imageRequestOperation.responseSerializer = self.imageResponseSerializer;
-        [self.af_imageRequestOperation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
-          __strong __typeof(weakSelf) strongSelf = weakSelf;
-          if ([[urlRequest URL] isEqual:[strongSelf.af_imageRequestOperation.request URL]]) {
-              if (success) {
-                  success(urlRequest, operation.response, responseObject);
-              } else if (responseObject) {
-                  dispatch_async(dispatch_get_main_queue(), ^(void) {
-                    strongSelf.image = responseObject;
-                  });
-              }
-
-              if (operation == strongSelf.af_imageRequestOperation) {
-                  strongSelf.af_imageRequestOperation = nil;
-              }
-          }
-          // cache the image recently fetched.
-          [cache cacheImage:responseObject forRequest:urlRequest];
-        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-          __strong __typeof(weakSelf) strongSelf = weakSelf;
-          if ([[urlRequest URL] isEqual:[strongSelf.af_imageRequestOperation.request URL]]) {
-              if (failure) {
-                  failure(urlRequest, operation.response, error);
-              }
-
-              if (operation == strongSelf.af_imageRequestOperation) {
-                  strongSelf.af_imageRequestOperation = nil;
-              }
+        completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
+          if (image) {
+              success(nil, nil, image);
           }
         }];
-        
-        if (downloadProgressBlock){
-            [self.af_imageRequestOperation setDownloadProgressBlock:downloadProgressBlock];
-        }
-        // get the NSoperationQueue associated With UIImageView class
-        #pragma clang diagnostic push
-        #pragma clang diagnostic ignored "-Wundeclared-selector"
-        NSOperationQueue * operationQueue =  (NSOperationQueue *)((id (*)(id, SEL))objc_msgSend)([self class], @selector(af_sharedImageRequestOperationQueue));
-        #pragma clang diagnostic pop
-        [operationQueue addOperation:self.af_imageRequestOperation];
-    }
 }
-
 
 @end
